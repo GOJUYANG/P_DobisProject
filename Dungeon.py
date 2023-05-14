631f3a471775f73d12cab47055868f91a451864e
Kim-jeje <alfajeze@gmail.com>
2023-05-14 14:00:55 +0900

    변수작성2

--- Dungeon.py
+++ Dungeon.py
@@ -1,90 +1,3 @@
-import random
-
-class Common():
-    def __init__(self, **kwargs):
-        if 'field' in kwargs:
-            self.field = kwargs['field']
-        if 'gard' in kwargs:
-            self.gard = kwargs['gard']
-        if 'monster' in kwargs:
-            self.moster = kwargs['monster']
-        if 'item' in kwargs:
-            self.item = kwargs['item']
-        if 'maze' in kwargs:
-            self.tower = kwargs['maze']
-
-        self.field_list = ['fire_area', 'water_area', 'forest_area', 'snow_area']
-        self.gard_list = ['light_gard', 'moon_gard', 'star_gard', 'forest_gard']
-        self.monster_list = ['fire_field_moster', 'water_field_moster', 'forest_field_moster', 'snow_field_moster']
-        self.item_list = ['meteorite', 'tent', 'HP_potion_high', 'HP_potion_middle', 'HP_potion_low', 'MP_potion_high',
-                          'MP_potion_middle', 'MP_potion_low', 'All_potion_high', 'All_potion_middle', 'All_potion_low',
-                          'survival']
-        self.move_drop_list = ['HP_potion_high', 'HP_potion_middle', 'HP_potion_low', 'MP_potion_high', 'MP_potion_middle',
-                               'MP_potion_low', 'All_potion_high', 'All_potion_middle', 'All_potion_low']
-        self.maze_list = ['maze_way', 'random_maze_way']
-        self.meteorite = ['meteorite_item']
-        self.user_state_list = ['job', 'lv', 'hp', 'mp', 'exp', 'skil', 'power']
-        # self.turn = 0
-
-    def random_field(self): # 랜덤 필드
-        rand_field_way = random.choice(self.field_list)
-        print(f"나의 용병단 {rand_field_way}의 위치에 생성")
-
-    def random_user_gard(self): # 랜덤 유저 속성 수호대
-        rand_user_gard = random.choice(self.gard_list)
-        print(f"나의 수호대 {rand_user_gard}로 생성")
-
-    def random_maze_start(self): # 랜덤한 위치에 던전 입구 생성
-        # if self.turn <= 10: # 10 턴 마다 던전입구 재생성
-        rand_maze_start = random.randint(0, 20)
-        rand_maze_start_ = random.randint(0, 20)
-        print(f"랜덤 던전 좌표 X {rand_maze_start} Y {rand_maze_start_}")
-
-    def random_meteorite_way(self): # 랜덤한 위치에 운석 생성
-        rand_met_way = random.randint(0, 20)
-        rand_met_way_ = random.randint(0, 20)
-        print(f"랜덤 운석 좌표 X {rand_met_way}, Y {rand_met_way_}")
-        # self.inven.append(self.meteorite)
-
-    def field_area(self): # 지역
-        print('\n')
-        for i in range(10):
-            print("'불'"*10,'"눈"'*10, end="\n")
-
-        for i in range(10):
-            print("'숲'"*10,'"물"'*10, end="\n")
-
-    def feild_move_random_drop(self): # 필드 이동 중 랜덤 드랍
-        drop_list = random.choice(self.move_drop_list)
-        print(drop_list)
-
-
-a = Common()
-a.random_field()
-b = Common()
-b.random_user_gard()
-c = Common()
-c.random_maze_start()
-d = Common()
-d.random_meteorite_way()
-e = Common()
-e.field_area()
-f = Common()
-f.feild_move_random_drop()
-
-# 시작 버튼 클릭 시 필드에서 4개지역 [불, 물, 숲, 눈] 중 랜덤한 위치에 수호대가 생성된다.0
-# 플레이어의 특정 수호대를 제외한 나머지 특정 타 수호대를 생성한다.
-# 방향키를 지정하여 맵을 이동한다 # 공통
-# 각 지역 이동 중 지역의 고유 몬스터를 일정 확률로 몬스터가 출몰한다.
-# 던전 입구는 필드 내 랜덤한 위치에 생성된다. 0
-# 이동 중 랜덤한 위치에 운석이 존재하며 획득한다. 0
-# 이동 중 일정 확률로 아이템 획득이 가능하다. (부활포션, 장비아이템 제외) 0
-# 이동 중 일정 확률로 타 수호대와 조우 한다.
-# 11번째 전투까지 던전 입구 못찾을시 랜덤한 위치에 재생성 !
-# 각 지역 필드 몬스터 설정
-# 각 지역 설정 0
-
-
 # #던전 입장
 # 던전 크기 랜덤 생성, 각 층의 던전 크기 저장
 
@@ -247,130 +160,173 @@ f.feild_move_random_drop()
 #         print('텔레포트를 사용해 필드({},{})로 이동'.format(self.tele_y, self.tele_x))
 
 import random
-#호할란아ㅣ닞ㄷㄹ
 #층별 면적 결정
-class DungeonArea():
+class DungeonClass():
     def __init__(self, **kwargs):
+        #던전입장 후 현재 층 : current_floor = 1
         if 'current_floor' in kwargs:
-            self.current_floor = kwargs['current_floor']
+            self.int_current_floor = kwargs['current_floor']
 
-        # self.teleport_stock = {'first': 5, 'second': 5, 'third': 5, 'fourth': 5, 'fifth': 5, 'sixth': 5, 'seventh': 5}
-        self.teleport_stock = {1: 5, 2: 5, 3: 5, 4: 5, 6: 5, 7: 5}
-        self.widths = random.choices([15, 16, 17, 18], k=7)
-        self.widths.append(4)
-        self.widths = tuple(self.widths)
-        print(self.widths)
+        self.dict_teleport_stock = {1: 5, 2: 5, 3: 5, 4: 5, 6: 5, 7: 5}
+        self.list_widths = random.choices([15, 16, 17, 18], k=7)
+        self.list_widths.append(4)
+        self.tuple_widths = tuple(self.list_widths)
+        print(self.tuple_widths)
 
-        self.first_width = self.widths[0]
-        self.second_width = self.widths[1]
-        self.third_width = self.widths[2]
-        self.fourth_width = self.widths[3]
-        self.fifth_width = self.widths[4]
-        self.sixth_width = self.widths[5]
-        self.seventh_width = self.widths[6]
-        self.eighth_width = self.widths[7]
+        self.first_width = self.tuple_widths[0]
+        self.second_width = self.tuple_widths[1]
+        self.third_width = self.tuple_widths[2]
+        self.fourth_width = self.tuple_widths[3]
+        self.fifth_width = self.tuple_widths[4]
+        self.sixth_width = self.tuple_widths[5]
+        self.seventh_width = self.tuple_widths[6]
+        self.eighth_width = self.tuple_widths[7]
 
-        # 클리어한 층(보스몬스터를 해치운 층)
-        self.cleared_floors = []
-        for i in range(1, self.current_floor):
-            self.cleared_floors.append(i)
-        # print(self.cleared_floors)
+    #일반몬스터 만남(1~10마리)
+    def meet_monster(self):
+        self.int_monster_count = random.randint(1, 10)
+
+    #아군 수호대 만남
+    def meet_army(self):
+        #9개의 포션, 장비들 중 하나 드랍
+        pass
+
+    #적군수호대 만남
+    def meet_enemy(self):
+        #전투시작
 
 
     # 던전 보스 위치
     def boss_location(self):
-        if self.current_floor == 1:
-            self.boss_x = random.randint(0, self.first_width-1)
-            self.boss_y = random.randint(0, self.first_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 2:
-            self.boss_x = random.randint(0, self.second_width-1)
-            self.boss_y = random.randint(0, self.second_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 3:
-            self.boss_x = random.randint(0, self.third_width-1)
-            self.boss_y = random.randint(0, self.third_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 4:
-            self.boss_x = random.randint(0, self.fourth_width-1)
-            self.boss_y = random.randint(0, self.fourth_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 5:
-            self.boss_x = random.randint(0, self.fifth_width-1)
-            self.boss_y = random.randint(0, self.fifth_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 6:
-            self.boss_x = random.randint(0, self.sixth_width-1)
-            self.boss_y = random.randint(0, self.sixth_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
-        elif self.current_floor == 7:
-            self.boss_x = random.randint(0, self.seventh_width-1)
-            self.boss_y = random.randint(0, self.seventh_width-1)
-            print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # if self.current_floor == 1:
+        #     self.boss_x = random.randint(0, self.first_width-1)
+        #     self.boss_y = random.randint(0, self.first_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 2:
+        #     self.boss_x = random.randint(0, self.second_width-1)
+        #     self.boss_y = random.randint(0, self.second_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 3:
+        #     self.boss_x = random.randint(0, self.third_width-1)
+        #     self.boss_y = random.randint(0, self.third_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 4:
+        #     self.boss_x = random.randint(0, self.fourth_width-1)
+        #     self.boss_y = random.randint(0, self.fourth_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 5:
+        #     self.boss_x = random.randint(0, self.fifth_width-1)
+        #     self.boss_y = random.randint(0, self.fifth_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 6:
+        #     self.boss_x = random.randint(0, self.sixth_width-1)
+        #     self.boss_y = random.randint(0, self.sixth_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        # elif self.current_floor == 7:
+        #     self.boss_x = random.randint(0, self.seventh_width-1)
+        #     self.boss_y = random.randint(0, self.seventh_width-1)
+        #     print("{}층({},{})에 보스".format(self.current_floor, self.boss_y, self.boss_x))
+        self.boss_x = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        self.boss_y = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        print("{}층 ({},{}) 보스몬스터".format(self.int_current_floor, self.boss_y, self.boss_x))
 
     # 보스 hp
     def boss_match(self):
-        self.boss_hp = 0
-        if self.current_floor == 1:
-            self.boss_hp = random.randint(25000, 35000)
-        elif self.current_floor == 2:
-            self.boss_hp = random.randint(45000, 55000)
-        elif self.current_floor == 3:
-            self.boss_hp = random.randint(65000, 75000)
-        elif self.current_floor == 4:
-            self.boss_hp = random.randint(75000, 85000)
-        elif self.current_floor == 5:
-            self.boss_hp = random.randint(85000, 599999)
-        elif self.current_floor == 6:
-            self.boss_hp = random.randint(999999, 9999999)
-        elif self.current_floor == 7:
-            self.boss_hp = 9999999
-        elif self.current_floor == 8:
+        self.int_boss_hp = 0
+        if self.int_current_floor == 1:
+            self.int_boss_hp = random.randint(25000, 35000)
+        elif self.int_current_floor == 2:
+            self.int_boss_hp = random.randint(45000, 55000)
+        elif self.int_current_floor == 3:
+            self.int_boss_hp = random.randint(65000, 75000)
+        elif self.int_current_floor == 4:
+            self.int_boss_hp = random.randint(75000, 85000)
+        elif self.int_current_floor == 5:
+            self.int_boss_hp = random.randint(85000, 599999)
+        elif self.int_current_floor == 6:
+            self.int_boss_hp = random.randint(999999, 9999999)
+        elif self.int_current_floor == 7:
+            self.int_boss_hp = 9999999
+        elif self.int_current_floor == 8:
             print('용사 복이 구출')
 
     # 텔레포트 사용
     def teleport_use(self):
-        if self.current_floor == 1:
-            self.teleport_stock[1] -= 1
-            print(self.teleport_stock[1])
-        elif self.current_floor == 2:
-            self.teleport_stock[2] -= 1
-            print(teleport_stock[2])
-        elif self.current_floor == 3:
-            self.teleport_stock[3] -= 1
-        elif self.current_floor == 4:
-            self.teleport_stock[4] -= 1
-        elif self.current_floor == 5:
-            self.teleport_stock[5] -= 1
-        elif self.current_floor == 6:
-            self.teleport_stock[6] -= 1
-        elif self.current_floor == 7:
-            self.teleport_stock[7] -= 1
+        if self.int_current_floor == 1:
+            self.dict_teleport_stock[1] -= 1
+            print(self.dict_teleport_stock[1])
+        elif self.int_current_floor == 2:
+            self.dict_teleport_stock[2] -= 1
+            print(self.dict_teleport_stock[2])
+        elif self.int_current_floor == 3:
+            self.dict_teleport_stock[3] -= 1
+        elif self.int_current_floor == 4:
+            self.dict_teleport_stock[4] -= 1
+        elif self.int_current_floor == 5:
+            self.dict_teleport_stock[5] -= 1
+        elif self.int_current_floor == 6:
+            self.dict_teleport_stock[6] -= 1
+        elif self.int_current_floor == 7:
+            self.dict_teleport_stock[7] -= 1
 
     # 텔레포트 사용시 필드 이동 위치
     def teleport_location(self):
         self.tele_x = random.randint(0, 19)
-        self.tele_y = random.randint(1, 19)
+        self.tele_y = random.randint(0, 19)
         print('텔레포트를 사용해 필드({},{})로 이동'.format(self.tele_y, self.tele_x))
 
     # 텔레포트 사용 후 재입장시 이동위치
     def come_back(self):
-        self.come_back_x = random.randint(0, self.widths[self.current_floor-1])
-        self.come_back_y = random.randint(0, self.widths[self.current_floor-1])
-        print('{}층 {},{}로 재입장'.format(self.current_floor, self.come_back_y, self.come_back_x))
+        self.come_back_x = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        self.come_back_y = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        print('{}층 {},{}로 재입장'.format(self.int_current_floor, self.come_back_y, self.come_back_x))
 
-    # 다음층 이동
+    # 보스 죽인후 다음층 이동
     def next_dungeon(self):
-        if self.current_floor == 1:
-            self.current_floor += 1
-            self.current_x = random.randint(0, self.widths[1]-1)
-            self.current_y = random.randint(0, self.widths[1]-1)
-            print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
-            # print(self.current_y)
+        # if self.current_floor == 1:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.second_width-1) #self.widths[self.current_floor]-1
+        #     self.current_y = random.randint(0, self.second_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        #     # print(self.current_y)
+        # elif self.current_floor == 2:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.third_width-1)
+        #     self.current_y = random.randint(0, self.third_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        # elif self.current_floor == 3:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.fourth_width-1)
+        #     self.current_y = random.randint(0, self.fourth_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        # elif self.current_floor == 4:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.fifth_width-1)
+        #     self.current_y = random.randint(0, self.fifth_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        # elif self.current_floor == 5:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.sixth_width-1)
+        #     self.current_y = random.randint(0, self.sixth_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        # elif self.current_floor == 6:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.seventh_width-1)
+        #     self.current_y = random.randint(0, self.seventh_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        # elif self.current_floor == 7:
+        #     self.current_floor += 1
+        #     self.current_x = random.randint(0, self.eighth_width-1)
+        #     self.current_y = random.randint(0, self.eighth_width-1)
+        #     print("{}층 {},{}에 입장".format(self.current_floor, self.current_y, self.current_x))
+        self.int_current_floor += 1
+        self.current_x = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        self.current_y = random.randint(0, self.tuple_widths[self.int_current_floor-1]-1)
+        print("{}층 {},{}로 진입".format(self.int_current_floor, self.current_y, self.current_x))
 
-DungeonArea(current_floor=1).next_dungeon()
-DungeonArea(current_floor=1).teleport_use()
-DungeonArea(current_floor=1).teleport_location()
-DungeonArea(current_floor=1).come_back()
-print(DungeonArea(current_floor=3).cleared_floors)
+    def boki_location(self):
+        if self.int_current_floor == 8:
+            self.int_boki_x = random.randint(0, 3)
+            self.int_boki_y = random.randint(0, 3)
+            print("{}층 {},{} 복이 구출".format(self.int_current_floor, self.int_boki_y, self.int_boki_x))
 
