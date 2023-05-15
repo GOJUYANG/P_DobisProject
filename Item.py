052d1d756b136567cf750c507cabc8095f963429
si-yeon <9gorgeous1@gmail.com>
2023-05-16 07:48:25 +0900

    아이템 획득

--- Item.py
+++ Item.py
@@ -3,7 +3,18 @@ class ItemClass():
         # 직업 리스트
         self.list_job = ['warrior', 'archer', 'swordsman', 'wizard_red', 'wizard_black', 'wizard_white']
 
-
+        # 아이템 리스트
+        self.dict_item = {'resurrection_potion': ['부활포션', 0, 'img_src/item/부활포션.png'],
+                          'tent': ['텐트', 0, 'img_src/item/텐트.png'],
+                          'all_potion_high': ['ALL포션(상)', 0, 'img_src/item/올3.png'],
+                          'all_potion_middle': ['ALL포션(중)', 0, 'img_src/item/올2.png'],
+                          'all_potion_low': ['ALL포션(하)', 0, 'img_src/item/올1.png'],
+                          'hp_potion_high': ['HP포션(상)', 0, 'img_src/item/빨간3.png'],
+                          'hp_potion_middle': ['HP포션(중)', 0, 'img_src/item/빨간2.png'],
+                          'hp_potion_low': ['HP포션(하)', 0, 'img_src/item/빨간1.png'],
+                          'mp_potion_high': ['MP포션(상)', 0, 'img_src/item/파란3.png'],
+                          'mp_potion_middle': ['MP포션(중)', 0, 'img_src/item/파란2.png'],
+                          'mp_potion_low': ['MP포션(하)', 0, 'img_src/item/파란1.png']}
 
     # 체력회복
     def use_hp_potion(self, str_job, str_item, dict_grad):
@@ -50,7 +61,10 @@ class ItemClass():
 
     # 아이템 획득
     def get_item(self):
-        pass
+        for equip in list_equip:
+            for k, v in self.dict_item.items():
+                if k == equip:
+                    v[1] += 1
 
     # 아이템창 갱신
     def renew_item_view(self):
