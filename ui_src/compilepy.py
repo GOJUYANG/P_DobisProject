6f527c3dc37db645a979903c60f23b66a1c95e92
김진영 <wlsdud3089@naver.com>
2023-05-23 12:29:17 +0900

    인트로 이미지 및 ui 수정1

--- ui_src/compilepy.py
+++ ui_src/compilepy.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 if __name__ == '__main__':
-    name = 'main_frame'
+    # name = 'main_frame'
     # name = 'equipment'
     # name = 'gard_name'
+    name = 'intro'
     os.system(f'python -m PyQt5.uic.pyuic -x {name}.ui -o {name}.py')
